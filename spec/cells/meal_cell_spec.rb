209ea58fade41ae2277ed29d903995b3a1ee8bff
Alex Saunders <acsauk@gmail.com>
2017-05-05 21:12:14 +0100

    adds pry-rails gem - creates cells test but syntax not quite right

--- spec/cells/meal_cell_spec.rb
+++ spec/cells/meal_cell_spec.rb
@@ -1,8 +1,13 @@
-describe MealCell, type: :cell do
-  subject do
-    cell(:meal, Meal.new(meal_slot: 'Morning',
-                         day_slot: 'Monday',
-                         user_id: 1))
+require 'rails_helper'
+
+RSpec.describe MealCell, type: :cell do
+
+  context 'cell rendering' do
+    context 'rendering show' do
+      subject { cell(:meal, Meal.new).call(:show) }
+
+      it { is_expected.to have_selector('a', text: 'Meal#show') }
+    end
   end
-  it { expect(subject).to have_content 'Meal#show' }
+
 end
