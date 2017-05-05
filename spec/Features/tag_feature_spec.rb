cae17d6739a4b398ad713a4f84e74567600fb507
Alex Saunders <acsauk@gmail.com>
2017-05-05 21:31:18 +0100

    creates failing integration test for adding tag to recipe

--- spec/Features/tag_feature_spec.rb
+++ spec/Features/tag_feature_spec.rb
@@ -1,7 +1,17 @@
-feature 'Tags' do
-  scenario 'add to recipes' do
-    sign_up
-    add_recipe
-    
+feature 'tags' do
+  context 'recipe added' do
+    before(:each) do
+      sign_up
+      add_recipe
+    end
+
+    it 'can add tag' do
+      click_link 'My Recipes'
+      click_link 'Omelette'
+      expect(page).to have_field('TagName')
+      fill_in 'TagName', with: 'Healthy'
+      click_button 'Add Tag'
+      expect(page).to have_content('Tag Successfully added to recipe')
+    end
   end
 end
