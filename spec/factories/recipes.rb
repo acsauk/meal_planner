4206050f2fb0872025b12db4714e1b69cac431c3
Alex Saunders <acsauk@gmail.com>
2017-06-12 16:09:01 +0100

    fixes typo to use instructions rather than description

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :recipe do
     title 'Chicken Cutlets'
-    description 'Description text that describes recipe steps goes here'
+    instructions 'Description text that describes recipe steps goes here'
   end
 end
