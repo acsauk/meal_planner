4d0e593e85aac7d1b95e03c7021ce6241bf7ecc6
Alex Saunders <acsauk@gmail.com>
2017-06-12 16:38:07 +0100

    adds database_cleaner to factory_girl setup to enable factory linting

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -3,5 +3,14 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     FactoryGirl.find_definitions
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+
+    begin
+      DatabaseCleaner.start
+      FactoryGirl.lint
+    ensure
+      DatabaseCleaner.clean
+    end
   end
 end
