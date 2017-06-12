506ee38522c5079a7d3a5ab5688cf04ab6773cd4
Alex Saunders <acsauk@gmail.com>
2017-06-12 17:02:32 +0100

    adds callback to instantiate recipe factory with ingredient via recipe_ingredient

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -2,5 +2,9 @@ FactoryGirl.define do
   factory :recipe do
     title 'Chicken Cutlets'
     instructions 'Description text that describes recipe steps goes here'
+
+    after(:create) do |recipe|
+      recipe.ingredients << FactoryGirl.build(:ingredient)
+    end
   end
 end
