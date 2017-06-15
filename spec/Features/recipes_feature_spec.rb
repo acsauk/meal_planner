bf8b31a735765019d11a892cf1b613182274005c
Alex Saunders <acsauk@gmail.com>
2017-06-15 15:16:26 +0100

    moves add recipe flow into a test and replaces hard coded values with FactoryGirl values. Deletes recipe_helper as its no longer required

--- spec/Features/recipes_feature_spec.rb
+++ spec/Features/recipes_feature_spec.rb
@@ -1,12 +1,21 @@
 feature 'Recipes' do
   before(:each) do
     sign_up
-    add_recipe
+    recipe_with_ingredients = FactoryGirl.build(:recipe_with_ingredients)
   end
 
   context 'user signed in' do
     scenario 'adding recipes' do
-      expect(page).to have_content 'Omelette recipe successfully created'
+      click_link 'Add recipe'
+      fill_in 'Title', with: recipe_with_ingredients.title
+      fill_in 'Instructions', with: recipe_with_ingredients.instructions
+      fill_in('recipe[ingredients_attributes][0][name]', with: recipe_with_ingredients.ingredients.first.name)
+      fill_in('recipe[ingredients_attributes][0][quantity]', with: recipe_with_ingredients.ingredients.first.quantity)
+      fill_in('recipe[ingredients_attributes][1][name]', with: recipe_with_ingredients.ingredients.second.name)
+      fill_in('recipe[ingredients_attributes][1][quantity]', with: recipe_with_ingredients.ingredients.second.quantity)
+      click_button 'Create Recipe'
+      expect(page).to have_content "#{recipe_with_ingredients.title} recipe successfully created"
+      expect(page).to have_link "#{recipe_with_ingredients.title}"
       expect(current_path).to eq '/recipes'
     end
 
