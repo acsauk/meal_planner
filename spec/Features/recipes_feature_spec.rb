bff7db3991c544f99e8950cb7a8ea3a3f53e736b
Alex Saunders <acsauk@gmail.com>
2017-06-28 14:33:32 +0100

    starts to redefine recipes spec following change to model and view for adding a recipe

--- spec/Features/recipes_feature_spec.rb
+++ spec/Features/recipes_feature_spec.rb
@@ -15,12 +15,14 @@ feature 'Recipes' do
     save_and_open_page
     fill_in 'Title', with: "#{saved_recipe.title} 2"
     fill_in 'Instructions', with: "#{saved_recipe.instructions} 2"
-    fill_in 'recipe[ingredients_attributes][0][name]', with: "#{saved_recipe.ingredients.first.name} 2"
-    fill_in 'recipe[ingredients_attributes][0][quantity]', with: "#{saved_recipe.ingredients.first.quantity} 2"
-    select 'grams', from: 'recipe[ingredients_attributes][0][unit]'
-    fill_in 'recipe[ingredients_attributes][1][name]', with: "#{saved_recipe.ingredients.second.name} 2"
-    fill_in 'recipe[ingredients_attributes][1][quantity]', with: "#{saved_recipe.ingredients.second.quantity} 2"
-    select 'grams', from: 'recipe[ingredients_attributes][1][unit]'
+    click_link 'add ingredient'
+    find('#recipe-ingredients .nested-fields:last-child input#name').set("#{saved_recipe.ingredients.first.name} 2")
+    # fill_in 'recipe[ingredients_attributes][0][name]', with: "#{saved_recipe.ingredients.first.name} 2"
+    # fill_in 'recipe[ingredients_attributes][0][quantity]', with: "#{saved_recipe.ingredients.first.quantity} 2"
+    # select 'grams', from: 'recipe[ingredients_attributes][0][unit]'
+    # fill_in 'recipe[ingredients_attributes][1][name]', with: "#{saved_recipe.ingredients.second.name} 2"
+    # fill_in 'recipe[ingredients_attributes][1][quantity]', with: "#{saved_recipe.ingredients.second.quantity} 2"
+    # select 'grams', from: 'recipe[ingredients_attributes][1][unit]'
     click_button 'Create Recipe'
     expect(page).to have_content "#{saved_recipe.title} 2 recipe successfully created"
     expect(page).to have_link "#{saved_recipe.title} 2"
