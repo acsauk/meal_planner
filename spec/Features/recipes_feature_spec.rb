dfda076ef6bedad807bad37afaf4fd0af5a6d4c8
Alex Saunders <acsauk@gmail.com>
2017-06-29 15:29:17 +0100

    uncomments tests, applies x to tests to skip them

--- spec/Features/recipes_feature_spec.rb
+++ spec/Features/recipes_feature_spec.rb
@@ -10,26 +10,26 @@ feature 'Recipes' do
     # click_button 'Log in'
   end
 
-  # scenario 'adding recipes', js:true do
-  #   visit '/'
-  #   click_link 'Add recipe'
-  #   fill_in 'Title', with: "#{saved_recipe.title} 2"
-  #   fill_in 'Instructions', with: "#{saved_recipe.instructions} 2"
-  #   click_link 'add ingredient'
-  #   find('#recipe-ingredients :nth-child(2) .nested_ingredient').set("#{saved_recipe.ingredients.first.name} 2")
-  #   find('#recipe-ingredients :nth-child(2) .nested_amount').set("#{saved_recipe.quantities.first.amount} 2")
-  #   find('#recipe-ingredients :nth-child(2) .nested_unit').set("#{saved_recipe.quantities.first.unit} 2")
-  #   click_link 'add ingredient'
-  #   find('#recipe-ingredients :nth-child(3) .nested_ingredient').set("#{saved_recipe.ingredients.first.name} 3")
-  #   find('#recipe-ingredients :nth-child(3) .nested_amount').set("#{saved_recipe.quantities.first.amount} 3")
-  #   find('#recipe-ingredients :nth-child(3) .nested_unit').set("#{saved_recipe.quantities.first.unit} 3")
-  #   click_button 'Create Recipe'
-  #   expect(page).to have_content "#{saved_recipe.title} 2 recipe successfully created"
-  #   expect(page).to have_link "#{saved_recipe.title} 2"
-  #   expect(current_path).to eq '/recipes'
-  # end
+  xscenario 'adding recipes', js:true do
+    visit '/'
+    click_link 'Add recipe'
+    fill_in 'Title', with: "#{saved_recipe.title} 2"
+    fill_in 'Instructions', with: "#{saved_recipe.instructions} 2"
+    click_link 'add ingredient'
+    find('#recipe-ingredients :nth-child(2) .nested_ingredient').set("#{saved_recipe.ingredients.first.name} 2")
+    find('#recipe-ingredients :nth-child(2) .nested_amount').set("#{saved_recipe.quantities.first.amount} 2")
+    find('#recipe-ingredients :nth-child(2) .nested_unit').set("#{saved_recipe.quantities.first.unit} 2")
+    click_link 'add ingredient'
+    find('#recipe-ingredients :nth-child(3) .nested_ingredient').set("#{saved_recipe.ingredients.first.name} 3")
+    find('#recipe-ingredients :nth-child(3) .nested_amount').set("#{saved_recipe.quantities.first.amount} 3")
+    find('#recipe-ingredients :nth-child(3) .nested_unit').set("#{saved_recipe.quantities.first.unit} 3")
+    click_button 'Create Recipe'
+    expect(page).to have_content "#{saved_recipe.title} 2 recipe successfully created"
+    expect(page).to have_link "#{saved_recipe.title} 2"
+    expect(current_path).to eq '/recipes'
+  end
 
-  scenario 'editing a recipe', js:true do
+  xscenario 'editing a recipe', js:true do
     visit '/recipes'
 
     click_link 'Edit recipe'
@@ -48,16 +48,16 @@ feature 'Recipes' do
     expect(page).to have_content "#{saved_recipe.quantities.first.unit} edit"
   end
 
-  # scenario 'deleting a recipe' do
-  #   visit '/recipes'
-  #   click_link 'Delete recipe'
-  #   expect(page).not_to have_content(saved_recipe.title.to_s)
-  # end
-  #
-  # scenario 'viewing a recipe' do
-  #   visit '/recipes'
-  #   click_link saved_recipe.title.to_s
-  #   expect(page).to have_content "#{saved_recipe.ingredients.first.quantity} #{saved_recipe.ingredients.first.name}"
-  #   expect(page).to have_content "#{saved_recipe.ingredients.second.quantity} #{saved_recipe.ingredients.second.name}"
-  # end
+  scenario 'deleting a recipe' do
+    visit '/recipes'
+    click_link 'Delete recipe'
+    expect(page).not_to have_content(saved_recipe.title.to_s)
+  end
+
+  xscenario 'viewing a recipe' do
+    visit '/recipes'
+    click_link saved_recipe.title.to_s
+    expect(page).to have_content "#{saved_recipe.ingredients.first.quantity} #{saved_recipe.ingredients.first.name}"
+    expect(page).to have_content "#{saved_recipe.ingredients.second.quantity} #{saved_recipe.ingredients.second.name}"
+  end
 end
