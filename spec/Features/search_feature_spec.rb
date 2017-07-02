c2c608cec07746d9bf99af63bb1cdd112c09dcd6
Alex Saunders <acsauk@gmail.com>
2017-07-02 13:53:54 +0100

    updates recipe factory to allow the provision of ingredient name. Updates search_feature_spec to test for the created recipes

--- spec/Features/search_feature_spec.rb
+++ spec/Features/search_feature_spec.rb
@@ -1,27 +1,28 @@
 feature 'search' do
-  let!(:saved_recipe) { FactoryGirl.create(:recipe_with_ingredients) }
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:saved_recipe) do
+    FactoryGirl.create(:recipe_with_ingredients, user: user)
+  end
 
   before(:each) do
+    login_as(user, scope: :user)
     visit '/'
-    click_link 'Sign in'
-    fill_in 'Email', with: saved_recipe.user.email
-    fill_in 'Password', with: saved_recipe.user.password
-    click_button 'Log in'
   end
 
   scenario 'by recipe titles' do
-    saved_recipe2 = FactoryGirl.create(:recipe_with_ingredients, title: 'Pasta')
-    fill_in 'q_title_or_ingredients_name_cont', with: saved_recipe.title
+    saved_recipe2 = FactoryGirl.create(:recipe_with_ingredients, title: 'Pasta', ingredient_name: 'spaghetti')
+    fill_in 'q_title_or_ingredients_name_cont', with: saved_recipe2.title
     click_button 'Search'
-    expect(page).to have_content(saved_recipe.title)
-    expect(page).not_to have_content(saved_recipe2.title)
+        binding.pry
+    expect(page).to have_content(saved_recipe2.title)
+    expect(page).not_to have_content(saved_recipe.title)
   end
 
   scenario 'by ingredients' do
-    saved_recipe2 = FactoryGirl.create(:recipe_with_ingredients, title: 'Pasta')
-    fill_in 'q_title_or_ingredients_name_cont', with: saved_recipe.ingredients.first.name
+    saved_recipe2 = FactoryGirl.create(:recipe_with_ingredients, title: 'Pasta', ingredient_name: 'spaghetti')
+    fill_in 'q_title_or_ingredients_name_cont', with: saved_recipe2.ingredients.first.name
     click_button 'Search'
-    expect(page).to have_content(saved_recipe.title)
-    expect(page).not_to have_content(saved_recipe2.title)
+    expect(page).to have_content(saved_recipe2.title)
+    expect(page).not_to have_content(saved_recipe.title)
   end
 end
