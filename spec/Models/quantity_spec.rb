a79b00950c765939175b3fcf54c126fb14cb182b
Alex Saunders <acsauk@gmail.com>
2017-07-03 14:48:05 +0100

    updates quantity spec to better describe what needs to happen with amount attribute

--- spec/Models/quantity_spec.rb
+++ spec/Models/quantity_spec.rb
@@ -2,7 +2,7 @@ describe Quantity, type: :model do
   it { is_expected.to belong_to(:ingredient) }
   it { is_expected.to belong_to(:recipe) }
 
-  it 'stores amount as Unitwise object' do
+  it 'amount attribute is returned as a Unitwise object' do
     quantity = FactoryGirl.build(:quantity)
     expect(quantity.amount).to be_a Unitwise
   end
