1812de8197f60ce27eb71dfef1bcbe952efd2c64
Alex Saunders <acsauk@gmail.com>
2017-07-03 15:29:53 +0100

    condenses spec wording

--- spec/Models/quantity_spec.rb
+++ spec/Models/quantity_spec.rb
@@ -2,13 +2,15 @@ describe Quantity, type: :model do
   it { is_expected.to belong_to(:ingredient) }
   it { is_expected.to belong_to(:recipe) }
 
-  it 'amount attribute is returned as a Unitwise::Measurement object' do
+  it 'amount_unit is returned as a Unitwise::Measurement object' do
     quantity = FactoryGirl.build(:quantity)
     expect(quantity.amount_unit).to be_a Unitwise::Measurement
   end
 
-  it 'amount attribute returns Unitwise::Measurement with initial amount' do
+  it 'amount_unit returns Unitwise::Measurement with initial amount' do
     quantity = FactoryGirl.build(:quantity)
     expect(quantity.amount_unit.value).to eq quantity.amount
   end
+
+  it
 end
