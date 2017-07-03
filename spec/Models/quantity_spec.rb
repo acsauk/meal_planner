25b96d5a56ff0952ee4dfef575d275265343c033
Alex Saunders <acsauk@gmail.com>
2017-07-03 14:06:56 +0100

    creates failing test for quantity.amount to be a Unitwise object

--- spec/Models/quantity_spec.rb
+++ spec/Models/quantity_spec.rb
@@ -1,4 +1,9 @@
 describe Quantity, type: :model do
   it { is_expected.to belong_to(:ingredient) }
   it { is_expected.to belong_to(:recipe) }
+
+  it 'stores amount as Unitwise object' do
+    quantity = FactoryGirl.build(:quantity)
+    expect(quantity.amount).to be_a Unitwise
+  end
 end
